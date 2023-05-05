2b9009b5445ddf188c6ecc96e445689e14090b40
etopritika <dimaprytyka@gmail.com>
2023-05-05 19:14:12 +0300

    init

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Section({title}) {
     return(
-        <>
+        <section>
             <h1>{title}</h1>
-        </>
+        </section>
     )
 }
