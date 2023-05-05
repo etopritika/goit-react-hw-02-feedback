78255b9bc66630d1eb1c882e35d9a1698104af4e
etopritika <dimaprytyka@gmail.com>
2023-05-05 19:13:14 +0300

    init

--- src/components/FeedbackOptions.jsx
+++ src/components/FeedbackOptions.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
 export default function FeedbackOptions({ options, onButtonClick }) {
-  console.log(options);
+  // console.log(options);
   return (
     <>
       <ul>
-        {/* {options.map((option, index) => (
+        {options.map((option, index) => (
           <li key={index}>
             <button type="button" onClick={() => onButtonClick(index)}>
               {option}
             </button>
           </li>
-        ))} */}
+        ))}
       </ul>
     </>
   );
