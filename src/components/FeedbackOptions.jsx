53836955cbd5c96731a0d59f2b7ac5ac70b5a61b
etopritika <dimaprytyka@gmail.com>
2023-05-08 19:03:26 +0300

    init

--- src/components/FeedbackOptions.jsx
+++ src/components/FeedbackOptions.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import css from '../styles/FeedbackOptions.module.css';
 
 export default function FeedbackOptions({ options, onButtonClick }) {
   return (
     <ul>
       {options.map((option, index) => (
         <li key={index}>
-          <button type="button" onClick={() => onButtonClick(option)}>
+          <button type="button" className={css.option__button} onClick={() => onButtonClick(option)}>
             {option}
           </button>
         </li>
